e55bdfba17859f3959887f11eec6ba986f5831a5
Craig Ford <craig_ford@icloud.com>
2022-12-06 19:27:49 -0500

    Initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+Friend.create!(name: "Al Colquitt", age: 43)
+Friend.create!(name: "Tom Och", age: 44)
+Friend.create!(name: "Jeff Egle", age: 45)
+Friend.create!(name: "Bryan Knotts", age: 46)
